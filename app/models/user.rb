8adc8b0c1abfde2fc5c7ca8301ee4785cacc5ff9
yyzcl <yyzclyang@gmail.com>
2020-07-18 12:53:11 +0800

    feat: 添加 user validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates_presence_of :email
+  # password 的检测 has_secure_password 做了
+  validates_presence_of :password_confirmation, on: [:create]
+
+  validates_format_of :email, with: /.+@.+/
+  validates_length_of :password, minimum: 6, on: [:create]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resource :users
+  # 等价于
+  # get '/users', to: 'users#index'
+  # get '/users/:id', to: 'users#show'
+  # post '/users', to: 'users#create'
+  # delete '/users/:id', to: 'users#destroy'
+  # patch '/users/:id', to: 'users#update'
+
 end
