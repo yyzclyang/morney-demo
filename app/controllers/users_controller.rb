dcaec4a2d25a411468f509cb9b98b73428fe0e3a
yyzcl <yyzclyang@gmail.com>
2020-07-18 22:52:36 +0800

    feat: 优化注册，保证邮箱的唯一性

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def create
     user = User.create(create_params)
     render_resource user
-    UserMailer.welcome_email(user).deliver_later
   end
 
   def create_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates_uniqueness_of :email
   validates_presence_of :email
   # password 的检测 has_secure_password 做了
   validates_presence_of :password_confirmation, on: [:create]
 
   validates_format_of :email, with: /.+@.+/, if: :email
   validates_length_of :password, minimum: 6, on: [:create], if: :password
+
+  after_create :send_welcome_email
+
+  def send_welcome_email
+    UserMailer.welcome_email(self).deliver_later
+  end
 end
