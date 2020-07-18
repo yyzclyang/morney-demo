cbfca5963f850f4556183f9cd2feee6e97d0fac7
yyzcl <yyzclyang@gmail.com>
2020-07-18 13:07:02 +0800

    feat: 添加 validate 国际化

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   # password 的检测 has_secure_password 做了
   validates_presence_of :password_confirmation, on: [:create]
 
-  validates_format_of :email, with: /.+@.+/
-  validates_length_of :password, minimum: 6, on: [:create]
+  validates_format_of :email, with: /.+@.+/, if: :email
+  validates_length_of :password, minimum: 6, on: [:create], if: :password
 end
