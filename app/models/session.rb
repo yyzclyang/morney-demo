16157448ee2c4b8518ddf351cfe4bfcda39c04f7
yyzcl <yyzclyang@gmail.com>
2020-07-19 21:41:20 +0800

    feat: 检验用户名与密码匹配

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,6 @@
 class Session
   include ActiveModel::Model
-  attr_accessor :email, :password
+  attr_accessor :email, :password, :user
 
   validates :email, presence: true
   validate :check_email if :email
@@ -8,11 +8,20 @@ class Session
 
   validates_format_of :email, with: /.+@.+/, if: :email
   validates_length_of :password, minimum: 6, if: :password
+  validate :email_password_match, if: Proc.new { |s| s.email.present? and s.password.present? }
 
   def check_email
-    user = User.find_by_email email
+    self.user = self.user || User.find_by_email(email)
     if user.nil?
       errors.add :email, :not_found
     end
   end
+
+  def email_password_match
+    self.user ||= User.find_by_email email
+    if user and not user.authenticate(password)
+      errors.add :password, :mismatch
+    end
+  end
+
 end
