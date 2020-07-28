8e11aea71d892dc647f62726ee37193a934b8a27
yyzcl <yyzclyang@gmail.com>
2020-07-28 21:45:45 +0800

    feat: 添加测试辅助函数

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,7 +32,21 @@ end
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+module SpecTestHelper
+  def sign_up(email, password, password_confirmation)
+    post '/users', params: {email: email, password: password, password_confirmation: password_confirmation}
+  end
+
+  def sign_in(email, password)
+    post '/sessions', params: {email: email, password: password}
+  end
+end
+
 RSpec.configure do |config|
+  # 表明 module SpecTestHelper 可以在 request 测试中使用
+  config.include SpecTestHelper, :type => :request
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -63,8 +77,8 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
 =begin
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
