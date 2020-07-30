8dfda2afb8420be899bb706a9b65246967a6101a
yyzcl <yyzclyang@gmail.com>
2020-07-30 22:25:15 +0800

    feat: 自定义 Error，当用户未登录时，报 401

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
+require 'custom_error'
+
 class ApplicationController < ActionController::API
+  rescue_from CustomError::MustSignInError, with: :render_must_sign_in
+
   # include ActionView::Layouts
   def current_user
     @current_user ||= User.find_by_id session[:current_user_id]
+  end
+
+  def must_sign_in
+    if current_user.nil?
+      raise CustomError::MustSignInError
+    end
+  end
+
+  def render_must_sign_in
+    render status: 401
   end
 end
