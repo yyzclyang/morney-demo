ac84eef05152416fc37dde6c8163103e90998bb2
yyzcl <yyzclyang@gmail.com>
2020-07-30 23:35:18 +0800

    feat: 增加 api document 的 sign_up sign_in 方法

--- spec/acceptance/record_acceptance_spec.rb
+++ spec/acceptance/record_acceptance_spec.rb
@@ -7,6 +7,7 @@ resource "Records" do
     parameter :category, '类型: outgoings | income', type: :string, required: true
     parameter :notes, '备注', type: :string
     example "创建账目记录" do
+      sign_in
       do_request(amount: 100, category: 'outgoings', notes: '吃饭')
 
       expect(status).to eq 200
--- spec/api_documentation_helper.rb
+++ spec/api_documentation_helper.rb
@@ -9,3 +9,25 @@ RSpec.configure do |config|
     end
   end
 end
+
+module RspecApiDocumentation::DSL
+  # DSL methods available inside the RSpec example.
+  module Endpoint
+    def sign_up(email, password, password_confirmation)
+      no_doc do
+        client.post '/users', {email: email, password: password, password_confirmation: password_confirmation}
+      end
+    end
+
+    def sign_in(email = nil, password = nil)
+      if email.nil? or password.nil?
+        email ||= 'spec_test_helper@qq.com'
+        password ||= '123456'
+        self.sign_up email, password, password
+      end
+      no_doc do
+        client.post '/sessions', {email: email, password: password}
+      end
+    end
+  end
+end
