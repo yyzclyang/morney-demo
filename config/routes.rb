c737a2e3aaef9e55141e6393875d53a3794cd1e9
yyzcl <yyzclyang@gmail.com>
2020-08-03 22:48:27 +0800

    fix: 修正路由的错误写法

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resource :users
+  resources :users
   # 等价于
   # get '/users', to: 'users#index'
   # get '/users/:id', to: 'users#show'
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   # delete '/users/:id', to: 'users#destroy'
   # patch '/users/:id', to: 'users#update'
 
-  resource :sessions, only: [:create]
+  resources :sessions, only: [:create]
   delete '/sessions', to: 'sessions#destroy'
 
-  resource :records
+  resources :records
 end
