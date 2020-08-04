fb9fba16e8af1be0fd8decc765fd94e13705debb
yyzcl <yyzclyang@gmail.com>
2020-08-04 22:28:56 +0800

    feat: 添加 record 查询的文档

--- spec/acceptance/record_acceptance_spec.rb
+++ spec/acceptance/record_acceptance_spec.rb
@@ -24,4 +24,30 @@ resource "Records" do
       expect(status).to eq 200
     end
   end
+
+  get "/records" do
+    parameter :page, '页码', type: :integer
+    let(:page) { 1 }
+    (1..11).each do
+      Record.create! amount: 1000, category: 'outgoings'
+    end
+    example "获取所有账目记录" do
+      sign_in
+      do_request
+
+      expect(status).to eq 200
+    end
+  end
+
+  get "/records/:id" do
+    parameter :id, '账目记录 id', type: :integer, require: true
+    let(:record) { Record.create! amount: 100, category: 'outgoings' }
+    let(:id) { record.id }
+    example "获取单个账目记录" do
+      sign_in
+      do_request
+
+      expect(status).to eq 200
+    end
+  end
 end
