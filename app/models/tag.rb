9a572581152095a02d585f1dbe5063add1a17bb6
yyzcl <yyzclyang@gmail.com>
2020-08-05 23:31:52 +0800

    feat: 完成 tag model 及其测试

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  validates_presence_of :name
+  validates_length_of :name, maximum: 30, if: :name
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it '可以创建 tag' do
+    tag = Tag.create name: '娱乐'
+    expect(tag.errors.empty?).to be true
+    expect(tag.id).to be_a Numeric
+  end
+  it '创建 tag 必须有 name' do
+    tag = Tag.create
+    expect(tag.errors.details[:name]).to include({:error => :blank})
+  end
+  it '创建 tag 的 name 最大长度为 30' do
+    tag = Tag.create name: '123456789012345678901234567890123'
+    expect(tag.errors.details[:name]).to include({:count => 30, :error => :too_long})
+  end
 end
